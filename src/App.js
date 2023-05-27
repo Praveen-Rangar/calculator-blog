b61e3abba2965f6d0a7f02be950f7259d332237a
Praveen-Rangar <praveenrangarkamand@gmail.com>
2023-05-27 22:33:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form>
+        <input className="" type="text" />
+      </form>
+
+      <div className="keypad">
+        <button className="highlights" id="clear">
+          Clear
+        </button>
+        <button className="highlights" id="backSpace">
+          C
+        </button>
+
+        <button className="highlights" name="/">
+          &divide;
+        </button>
+
+        <button name="7">7</button>
+        <button name="8">8</button>
+        <button name="9">9</button>
+        <button className="highlights" name="*">
+          &times;
+        </button>
+        <button name="4">4</button>
+        <button name="5">5</button>
+        <button name="6">6</button>
+        <button className="highlights" name="-">
+          &ndash;
+        </button>
+        <button name="1">1</button>
+        <button name="2">2</button>
+        <button name="3">3</button>
+        <button className="highlights" name="+">
+          +
+        </button>
+        <button name="0">0</button>
+        <button name=".">.</button>
+        <button className="highlights" id="result">
+          =
+        </button>
+      </div>
     </div>
   );
 }
